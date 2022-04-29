e9b207b442139e974ea218740a5ee1d465cc9d55
Aaron Sapp <sapp.aj@pg.com>
2022-04-29 15:51:42 -0400

    Initial commit

---
+++ Test/ApplangaTest/Applanga_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Applanga_TestApp.swift
+//  Applanga Test
+//
+//  Created by Aaron Sapp on 1/27/23.
+//
+
+import SwiftUI
+
+@main
+struct Applanga_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
--- step.sh
+++ step.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-set -ex
 
 if [ -z "${path_to_project}" ] ; then
   echo " [!] Missing required input: path_to_project"
@@ -9,7 +8,24 @@ fi
 
 cd "${path_to_project}"
 
-# TODO: check for periphery file. If not, check parameters to build the scan
+if [[ ! -f ".applanga.json" ]]
+then
+    echo ".applanga.json file is missing"
 
-output="$(periphery scan --quiet --format checkstyle)"
-envman add --key "PERIPHERY_REPORT" --value "${output}"
+    exit 1
+fi
+
+
+COMMAND="${command}"
+
+if [ "${force}" = "true" ]; then
+    COMMAND="$COMMAND --force"
+fi
+
+if [ "${draft}" = "true" ]; then
+    COMMAND="$COMMAND --draft"
+fi
+
+echo "Running 'applanga ${COMMAND}'"
+
+applanga $COMMAND
